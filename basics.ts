c8cacc6434c0724684fd55c5145fae78768b9ff2
Tim Waite <tim@dialexa.com>
2020-09-08 09:40:18 -0500

    Add basics

--- basics.ts
+++ basics.ts
@@ -3,10 +3,15 @@ import { format } from 'date-fns'
 
 const el = document.getElementById('app');
 
+interface hookState {
+  deps?: any[];
+  value?: any;
+} 
+
 const React = (function () {
   let rootEl: HTMLElement;
   let rootComponent;
-  let hooks = [];
+  let hooks = [] as hookState[];
   let currentHook = 0;
 
   const reactRender = () => {
@@ -20,37 +25,9 @@ const React = (function () {
     reactRender();
   };
 
-  const useEffect = (callback, depArray) => {
-    const hasNoDeps = !depArray;
-    const deps = hooks[currentHook]; // type: array | undefined
-    const hasChangedDeps = deps ? !depArray.every((el, i) => el === deps[i]) : true;
-    if (hasNoDeps || hasChangedDeps) {
-      callback();
-      hooks[currentHook] = depArray;
-    }
-    currentHook++; // done with this hook
-  };
-
-  const useState = (initialValue) => {
-    hooks[currentHook] = hooks[currentHook] || initialValue; // type: any
-    const setStateHookIndex = currentHook; // for setState's closure!
-    const setState = newState => (hooks[setStateHookIndex] = newState);
-    return [hooks[currentHook++], setState];
-  };
-
-  const useMemo = (memo, depArray) => {
-    const hasNoDeps = !depArray;
-    var [value, deps] = hooks[currentHook] ?? []; // type: array | undefined
-    const hasChangedDeps = deps ? !depArray.every((el, i) => el === deps[i]) : true;
-
-    if (hasNoDeps || hasChangedDeps) {
-      value = memo()
-    }
-
-    hooks[currentHook++] = [value, depArray]
-
-    return value;
-  }
+  const useEffect = () => {};
+  const useState = () => {};
+  const useMemo = () => {}
 
   return {
     mount,
@@ -62,49 +39,15 @@ const React = (function () {
 })();
 
 const Component = () => {
-  var [count, setCount] = React.useState(0);
-  var [date, setDate] = React.useState(new Date());
 
-  React.useEffect(() => {
-    console.log('hello this is an effect')
-  }, []);
-
-  React.useEffect(() => {
-    var timeout = setTimeout(() => {
-      setDate(new Date());
-      React.render();
-    }, 1000);
-
-    return () => clearTimeout(timeout);
-  }, [date, setDate])
-
-  const doubleCount = React.useMemo(() => {
-    console.log('calculating doubleCount');
-    return count * 2;
-  }, [count])
-
-  const increment = () => {
-    setCount(count + 1);
-    React.render();
-  }
-
-  const formatTime = (date: Date) => {
-    return format(date, 'HH:mm:ss a')
+  const log = () => {
+    console.log('hello world');
   }
 
   return html`
-    <div class="h-screen w-100 flex flex-col bg-gray-700 items-center justify-center">
-      <div class="rounded bg-gray-100 w-2/3 px-6 py-4 flex flex-col shadow-lg">
-        <h1 class="text-3xl">What the Hook?!</h1>  
-        <h3 class="text-xl">Count: ${count}</h3>
-        <h3 class="text-xl">Double count: ${doubleCount}</h3>
-        <button
-          @click=${increment}
-          class="rounded hover:bg-pink-700 bg-pink-600 p-2 text-white mt-2">
-          Increment
-        </button>
-      </div>
-      <span class="text-3xl text-white m-2 fixed right-0 bottom-0">${formatTime(date)}</span>
+    <div class="bg-white p-4 rounded">
+      <h1>My Component</h1>
+      <button @click=${log} class="bg-pink-700 text-white rounded py-2 px-3">Log</button>
     </div>
   `;
 }
